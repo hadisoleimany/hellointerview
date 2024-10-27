387af1622b277e2c2126f081f4aec3e4f71c951f
Hadi <hsoleymani@bol.com>
2024-10-27 15:14:32 +0100

    solve two sum with two_pointer without nested loop

--- src/main/java/org/hellointerview/TwoPointerTechnique.java
+++ src/main/java/org/hellointerview/TwoPointerTechnique.java
@@ -1,4 +1,32 @@
 package org.hellointerview;
 
 public class TwoPointerTechnique {
+
+    //with two nested for
+    public boolean twoSum(int[] num, int target) {
+        for (int i = 0; i < num.length; i++) {
+            int firstNum = num[i];
+            for (int j = i + 1; j < num.length; j++) {
+                if (firstNum + num[j] == target)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean twoSumWithoutNestedLoop(int[] num, int target) {
+        int leftPointer = 0, rightPointer = num.length - 1;
+        while (leftPointer < rightPointer) {
+            int left = num[leftPointer];
+            int right = num[rightPointer];
+            int sum = left + right;
+            if (sum == target) {
+                return true;
+            } else if (sum < target) {
+                leftPointer++;
+            } else
+                rightPointer--;
+        }
+        return false;
+    }
 }
--- src/test/java/org/hellointerview/TwoPointerTechniqueTest.java
+++ src/test/java/org/hellointerview/TwoPointerTechniqueTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.hellointerview;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class TwoPointerTechniqueTest {
-  
+    TwoPointerTechnique twoPointer=new TwoPointerTechnique();
+    int[] numbs= new int[]{1,3,4,6,8,10,13};
+    @Test
+    void testTwoSum(){
+        int target = 13;
+        assertTrue(twoPointer.twoSum(numbs, target));
+        assertTrue(twoPointer.twoSumWithoutNestedLoop(numbs, target));
+
+    }
+    @Test
+    void testTwoSum2(){
+         int target = 6;
+        assertFalse(twoPointer.twoSum(numbs, target));
+        assertFalse(twoPointer.twoSumWithoutNestedLoop(numbs, target));
+    }
 }
